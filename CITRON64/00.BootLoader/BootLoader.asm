ff719cee66fe8d43bdde965ea475500523fe0bdb
citron0137 <citron0137@gmail.com>
2018-12-21 19:01:36 +0900

    124p

--- CITRON64/00.BootLoader/BootLoader.asm
+++ CITRON64/00.BootLoader/BootLoader.asm
@@ -3,6 +3,12 @@
 
 SECTION .text
 
+mov ax, 0xB800
+mov ds, ax
+
+mov byte[0x00], 'M'
+mov byte[0x01], 0x4A
+
 jmp $
 
 times 510 - ($ - $$) db 0x00
---
+++ CITRON64/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+qemu-system-x86_64 -L . -m 64 -fda Disk.img -localtime -M pc
