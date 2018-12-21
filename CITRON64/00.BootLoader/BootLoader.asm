9a7ef3d30515ae520e3797d63b97508a2b804aff
citron0137 <citron0137@gmail.com>
2018-12-21 19:20:24 +0900

    132p

--- CITRON64/00.BootLoader/BootLoader.asm
+++ CITRON64/00.BootLoader/BootLoader.asm
@@ -3,13 +3,44 @@
 
 SECTION .text
 
-mov ax, 0xB800
-mov ds, ax
+jmp 0x07C0:START
 
-mov byte[0x00], 'M'
-mov byte[0x01], 0x4A
+START:
+    mov ax, 0x07C0
+    mov ds, ax
+    mov ax, 0xB800
+    mov es, ax
 
-jmp $
+    mov si, 0
+
+.SCREENCLEARLOOP:
+    mov byte[es:si], 0
+    mov byte[es:si+1], 0x0A
+
+    add si, 2
+
+    cmp si, 80*25*2
+
+    jl .SCREENCLEARLOOP
+
+    mov si, 0
+    mov di, 0
+
+.MESSAGELOOP:
+    mov cl, byte[si+MESSAGE1]
+    cmp cl, 0
+    je .MESSAGEEND
+
+    mov byte[es:di], cl
+
+    add si,1
+    add di,2
+    jmp .MESSAGELOOP
+
+.MESSAGEEND:
+    jmp $
+
+MESSAGE1: db 'CITRON64 OS Boot Loader Start~!!', 0
 
 times 510 - ($ - $$) db 0x00
 
