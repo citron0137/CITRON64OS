51a34569d2e65ef985d95368cf64a8942e37e522
citron0137 <citron0137@gmail.com>
2018-12-23 01:24:10 +0900

    234p (initialize IA-32e area)

--- CITRON64/01.Kernel32/Source/Main.c
+++ CITRON64/01.Kernel32/Source/Main.c
@@ -1,9 +1,13 @@
 #include "Types.h"
 
 void kPrintString( int iX, int iY, const char* pcString );
+BOOL kInitializeKernel64Area(void);
 
 void Main( void ){
     kPrintString(0, 3, "C Lang Kernel Started");
+    kInitializeKernel64Area();
+    kPrintString(0, 4, "IA-32e Kernel Area Initialization Complete");
+
     while(1);
 }
 
@@ -14,4 +18,20 @@ void kPrintString(int iX, int iY, const char * pcString ){
     for(i = 0; pcString[i]!=0; i++){
         pstScreen[i].bCharactor = pcString[i];
     } 
+}
+
+BOOL kInitializeKernel64Area(void){
+    DWORD *pdwCurrentAddress;
+    
+    pdwCurrentAddress = (DWORD *) 0x100000;
+
+    while((DWORD)pdwCurrentAddress < 0x600000){
+        *pdwCurrentAddress = 0x00;
+
+        if(*pdwCurrentAddress != 0x00){
+            return FALSE;
+        }
+        pdwCurrentAddress++;
+    }
+    return TRUE;
 }
