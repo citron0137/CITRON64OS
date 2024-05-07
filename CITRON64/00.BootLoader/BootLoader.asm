2a9dc8da3f5bd6a5bb257ec0c18ee5d13f23c066
rado <citron0137@gmail.com>
2024-05-07 23:52:04 +0900

    Add Clearing display

--- CITRON64/00.BootLoader/BootLoader.asm
+++ CITRON64/00.BootLoader/BootLoader.asm
@@ -3,13 +3,45 @@
 
 SECTION .text       ; text섹션 정의
 
-mov ax, 0xB800      
-mov ds, ax          ; DS 레지스터에 0xB800 (비디오 메모리 어드레스) 지정
+jmp 0x07C0:START    ; 0x07C0(부트로더 영역)으로 CS 레지스터 지정 및 START로 이동
 
-mov byte [ 0x00 ], 'C'
-mov byte [ 0x01 ], 0x06 ; 검은색 배경에 노란색 글자 
+START:
+    mov ax, 0x07C0
+    mov ds, ax      ; 부트로더의 시작 주소를 DS 레지스터에 저장  
+    mov ax, 0xB800      
+    mov es, ax      ; ES 레지스터에 0xB800 (비디오 메모리 어드레스) 지정
+    mov si, 0       ; SI 레지스터(문자열 원본 인덱스 레지스터) 초기화    
 
-jmp $               ; 무한 루프 수행
+.SCREENCLEARLOOP:
+    mov byte [ es: si ], 0          ; 화면에 아무 문자도 안보이게끔
+    mov byte [ es: si + 1 ], 0x06   ; 글자서식을 검정바탕에 노란색으로 지정
+    
+    add si, 2
+    cmp si, 80 * 25 *2
+    jl .SCREENCLEARLOOP
+
+
+; ScreenClearLoop End
+
+    mov si, 0
+    mov di, 0
+.MESSAGELOOP:
+    mov cl, byte [ MESSAGE1 + si ]  ; cl에 메시지의 si번째 문자 복사
+
+    cmp cl, 0                       ; 문자열 끝인지 비교
+    je .MESSAGEEND
+
+    mov byte [ es: di ], cl         ; 비디오 메모리에 문자 출력
+
+    add si, 1                       ; 문자는 1Byte씩 카운팅
+    add di, 2                       ; 비디오 메모리는 2Byte 씩 넘어가기
+    jmp .MESSAGELOOP
+
+.MESSAGEEND:
+
+    jmp $
+
+MESSAGE1: db "CITRON64 OS BootLoader Let's go!", 0 
 
 times 510 - ( $ - $$ )    db  0x00    ;510바이트를 00으로 채움 
 
