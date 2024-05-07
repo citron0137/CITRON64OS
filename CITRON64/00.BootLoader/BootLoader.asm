4d45a48fdb29a2659a78aee62c78631a279617e4
rado <citron0137@gmail.com>
2024-05-08 01:00:14 +0900

    Add Image Loading

--- CITRON64/00.BootLoader/BootLoader.asm
+++ CITRON64/00.BootLoader/BootLoader.asm
@@ -50,7 +50,7 @@ START:
     call PRINTMESSAGE
     add sp, 6
     
-; 디스크 리셋 Step 1. 디스크 초기화
+; 디스크 로딩 Step 1. 디스크 초기화
 RESETDISK:
     ; BIOS RESET FUNCTION 호출
     mov ax, 0   ; 서비스 번호 0
@@ -58,6 +58,54 @@ RESETDISK:
     int 0x13
     jc HANDLEDISKERROR  ; 에러가 발생한 경우 핸들러로 이동
 
+; 디스크 로딩 Step 2. 디스크 섹터 읽기
+    mov si, 0x1000         
+    mov es, si          
+    mov bx, 0x00000      ; 0x10000에 복사하기 위해 es, bx 지정 
+    mov di, word [ TOTALSECTORCOUNT ]   ; 복사할 총 섹터수를 di에 저장
+
+READDATA:
+    cmp di, 0       
+    je READEND          ; 모든 디스크를 다 읽은 경우 READEND로 이동
+    sub di, 0x01
+
+    ; BIOS READ Function으로 읽기
+    mov ah, 0x02                    ; 서비스 번호 2 (Read Sector)
+    mov al, 0x1                     ; 1개의 섹터 읽기
+    mov ch, byte [ TRACKNUMBER ]    ; 읽을 트랙 번호 지정
+    mov cl, byte [ SECTORNUMBER ]   ; 읽을 섹터 번호 지정
+    mov dh, byte [ HEADNUMBER ]     ; 읽을 헤드 번호 지정
+    mov dl, 0x00                    ; 읽을 드라이브 번호 0(Floppy) 지정
+    int 0x13
+    jc HANDLEDISKERROR              ; 에러 발생 확인
+
+    ; 다음으로 읽을 어드레스, 트랙, 헤드, 섹터 계산
+    add si, 0x0020                  ; 한번에 512(0x200)바이트씩 읽음, es에 더하기 위함
+    mov es, si
+
+    mov al, byte [ SECTORNUMBER ]   ; 읽은 섹터 번호 저장
+    mov al, 0x01                    ; 하나 증가
+    mov byte [ SECTORNUMBER ], al   ; 다시 저장
+    cmp al, 19  
+    jl READDATA                     ; 19보다 작으면 다시 읽기
+
+    xor byte [ HEADNUMBER ], 0x01   ; 헤드 번호를 토글하기
+    mov byte [ SECTORNUMBER ], 0x01 ; 섹터번호를 다시 1로 설정
+
+    cmp byte [ HEADNUMBER ], 0x00   ; 헤드 넘버가 0->1 으로 변경된 경우 
+    jne READDATA                    ; 별다른 처리 없이 다시 읽기
+
+    ; 트랙을 1 증가시킨 후 다시 섹터 읽기로 이동
+    add byte [ TRACKNUMBER ], 0x01  ; 트랙번호 1증가
+    jmp READDATA                    ; 다시 읽기
+READEND:
+
+; 디스크 로딩 Step 3(마지막). 성공 메시지 출력
+    push LOADINGCOMPLETEMESSAGE
+    push 1
+    push 16
+    call PRINTMESSAGE
+    add sp, 6
 
 ; 무한 대기 상태 
     jmp $
@@ -131,12 +179,21 @@ PRINTMESSAGE:
 ; PRINTMESSAGE 종료 
     
 
-MESSAGE1:               db "CITRON64 OS BootLoader Let's go!", 0 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 데이터 영역 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; 상수들
+MESSAGE1:               db "CITRON64 OS BootLoader, Let's go!", 0 
 IMAGELOADINGMESSAGE:    db "Image Loading...", 0
-DISKERRORMESSAGE:       db "Fail, Disk Error", 0
+DISKERRORMESSAGE:       db " Fail, Disk Error", 0
+LOADINGCOMPLETEMESSAGE: db " Success!", 0
+
+; 변수들
+SECTORNUMBER:           db 0x02
+HEADNUMBER:             db 0x00
+TRACKNUMBER:            db 0x00 
 
 times 510 - ( $ - $$ )    db  0x00    ;510바이트를 00으로 채움 
 
 db 0x55             ; 
 db 0xAA             ; 0x55 0xAA로 부트섹터를 표시함
-
