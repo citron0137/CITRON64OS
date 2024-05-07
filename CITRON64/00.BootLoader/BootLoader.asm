e4f567e6c1061491dffec523ab74dbf700f0e0b7
rado <citron0137@gmail.com>
2024-05-07 23:19:39 +0900

    Add Printing single letter

--- CITRON64/00.BootLoader/BootLoader.asm
+++ CITRON64/00.BootLoader/BootLoader.asm
@@ -3,6 +3,12 @@
 
 SECTION .text       ; text섹션 정의
 
+mov ax, 0xB800      
+mov ds, ax          ; DS 레지스터에 0xB800 (비디오 메모리 어드레스) 지정
+
+mov byte [ 0x00 ], 'C'
+mov byte [ 0x01 ], 0x06 ; 검은색 배경에 노란색 글자 
+
 jmp $               ; 무한 루프 수행
 
 times 510 - ( $ - $$ )    db  0x00    ;510바이트를 00으로 채움 
