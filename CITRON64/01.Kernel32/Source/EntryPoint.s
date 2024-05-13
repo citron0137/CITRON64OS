b138f3e538dbbdabfe4380b350ec87aad687b64b
rado <citron0137@gmail.com>
2024-05-13 21:06:15 +0900

    WIP: Might have issue in 32bit entrypoint line 27

--- CITRON64/01.Kernel32/Source/EntryPoint.s
+++ CITRON64/01.Kernel32/Source/EntryPoint.s
@@ -12,11 +12,9 @@ START:
     mov ax, 0x1000
     mov es, ax          ; es를 현재 메모리 주소 기준으로 계산
 
-    jmp $   ; 무한 반복 (임시 코드)
-
     cli                 ; 인터럽트가 발생하지 않도록 설정
     lgdt [ GDTR ]       ; GDT 로드
-
+    
     ; 보호모드 진입
     ; 4 => PG=0 (페이지 사용 x), CD=1 (캐시 비활성화), NW=0 (write-through), 
     ; 0 => AM=0(어드레스 배수 체크 x), WP=0, 
@@ -24,13 +22,14 @@ START:
     ; b => TS=1(테스크 전환되었음?), EM=0(프로세서에 FPU 내장 x),  MP=1 (wait시 TS가 1이면 예외발생), PE=1 (보호모드 진입!) 
     mov eax, 0x4000003B ; PG=0, CD=1, NW=0, AM=0, WP=0, NE=1, ET=1, TS=1, EM=0, MP=1, PE=1
     mov cr0, eax
-
+    
     ; CS 및 eip 재설정 (CodeSegment, CodeSegment기준 주소)
-    jmp dword 0x08: ( PROTECTEMODE - $$ + 0x10000 )
+    jmp dword 0x08: ( PROTECTEDMODE - $$ + 0x10000 )
 
 
 [BITS 32]
-PROTECTEMODE:
+PROTECTEDMODE:
+    
     mov ax, 0x10
     mov ds, ax
     mov es, ax
@@ -38,9 +37,61 @@ PROTECTEMODE:
     mov gs, ax 
     ; 세그멘트를 모두 DataSegement로 세팅
 
+    mov ss, ax
+    mov esp, 0xFFFE     ; 스택 초기화
+    mov ebp, 0xFFFE     ; 스택 초기화
+
+    push ( SWITCHSUCCESSMESSAGE - $$ + 0x10000 )
+    push 2
+    push 0 
+    call PRINTMESSAGE
+    add esp, 12
+
     jmp $
 
 ; 함수
+
+; 출력 함수 (x, y, 문자열)
+PRINTMESSAGE:
+    push ebp
+    mov ebp, esp
+    push esi
+    push edi 
+    push eax
+    push ecx
+    push edx
+
+    mov eax, dword [ ebp + 12 ]     ; pos Y
+    mov esi, 160
+    mul esi                         
+    mov edi, eax
+
+    mov eax, dword [ ebp + 8 ]      ; pos X
+    mov esi, 2
+    mul esi
+    add edi, eax                    ; posY * 160 + posX = 메모리어드레스상 주소
+
+    mov esi, dword [ ebp + 16 ]     ; 문자열 포인터
+    
+.MESSAGELOOP:
+    mov cl, byte [ esi ]    ;한 글자 가져오기
+    
+    cmp cl, 0
+    je .MESSAGEEND          ; 마지막 문자면 끝으로 이동
+
+    mov byte [ edi + 0xB8000 ], cl ; 문자열 출력
+
+    add esi, 1
+    add edi, 2
+
+.MESSAGEEND:
+    pop edx
+    pop ecx
+    pop eax
+    pop edi
+    pop esi
+    pop ebp
+    ret
 
 ;;;;;;;;;;;;;;;;;
 ; 데이터 영역
@@ -91,6 +142,8 @@ GDT:
         db 0xCF     ; G=1, D/B=1, L=0, limit[19:16]
         db 0x00     ; base[31:24]
 GDTEND:
+
+SWITCHSUCCESSMESSAGE: db 'Switched to protected mode', 0
 
 
 times 512 - ( $ - $$ ) db 0x00 ; 512 채우기
