0be251cac064c0feb1b3f071b3d1dc4edee9d9e8
rado <citron0137@gmail.com>
2024-05-20 21:42:59 +0900

    Fix Bug in 32bit Kernel

--- CITRON64/01.Kernel32/Source/Main.c
+++ CITRON64/01.Kernel32/Source/Main.c
@@ -9,10 +9,9 @@ void Main( void ){
 
 void kPrintString( int iX, int iY, const char* pcString )
 {
-    CHARACTER* pstScreen = ( CHARACTER* ) 0xB800; 
-    int i;
+    CHARACTER* pstScreen = ( CHARACTER* ) 0xB8000; 
     pstScreen += ( iY * 80 ) + iX;
-    for( i = 0; pcString[i] != 0; i++ )
+    for( int i = 0; pcString[i] != 0; i++ )
     {
         pstScreen[ i ].bCharactor = pcString[ i ];
     }
---
+++ Debugger/debug32.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+make -C ../CITRON64 clean
+make -C ../CITRON64
+
+qemu-system-i386 -L . -m 64 -fda ../CITRON64/Disk.img -M pc -s -S &
+
+sleep 1s
+
+gdb -q \
+	-ex "target remote :1234" \
+	-ex "set disassembly-flavor intel" \
+	-ex "layout asm" \
+	-ex "layout reg" \
+	-ex "b *0x10200" \
+	-ex "c" 
+	
+	# -ex "set tdesc filename target.xml" \
