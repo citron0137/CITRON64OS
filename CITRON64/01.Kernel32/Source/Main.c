d8992ebd0887b55b22c8b5b0ab3fc6161d88ed1f
rado <citron0137@gmail.com>
2024-05-26 21:59:59 +0900

    Fix Bug

--- CITRON64/01.Kernel32/Source/Main.c
+++ CITRON64/01.Kernel32/Source/Main.c
@@ -36,8 +36,7 @@ void Main( void ){
     while(1);
 }
 
-void kPrintString( int iX, int iY, const char* pcString )
-{
+void kPrintString( int iX, int iY, const char* pcString ){
     CHARACTER* pstScreen = ( CHARACTER* ) 0xB8000; 
     pstScreen += ( iY * 80 ) + iX;
     for( int i = 0; pcString[i] != 0; i++ )
@@ -45,21 +44,19 @@ void kPrintString( int iX, int iY, const char* pcString )
         pstScreen[ i ].bCharactor = pcString[ i ];
     }
 }
-void kPrintStringWithCheckBox( int iY, const char* pcString )
-{
+void kPrintStringWithCheckBox( int iY, const char* pcString ){
     kPrintString( 0, iY, "............................................[    ]"); 
     kPrintString( 0, iY, pcString );
 }
 
-void kFillCheckBox( int iY, const char* pcString )
-{
+void kFillCheckBox( int iY, const char* pcString ){
     kPrintString( 45, iY, pcString );
 }
 
 BOOL kInitializeKernel64Area( void ){
     DWORD* pdwCurrentAddress;
     pdwCurrentAddress = ( DWORD* ) 0x100000;
-    while( (DWORD*) pdwCurrentAddress <= 0x600000 ){
+    while( pdwCurrentAddress <= ( DWORD* ) 0x600000 ){
         *pdwCurrentAddress = 0x00;
         if( *pdwCurrentAddress != 0 ) return FALSE;
         pdwCurrentAddress++;
@@ -68,5 +65,11 @@ BOOL kInitializeKernel64Area( void ){
 }
 
 BOOL kIsMemoryEnough( void ){
-
+    DWORD* pdwCurrentAddress = ( DWORD* ) 0x100000;
+    while( pdwCurrentAddress <= ( DWORD* ) 0x400000 ){
+        *pdwCurrentAddress = 0x12345678;
+        if( *pdwCurrentAddress != 0x12345678 ) return FALSE;
+        pdwCurrentAddress += ( 0x100000 / 4 );
+    }
+    return TRUE;
 }
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 make clean
 make 
-qemu-system-x86_64 -L . -m 64 -fda ./CITRON64/Disk.img -M pc
+qemu-system-x86_64 -L . -m 64m -fda ./CITRON64/Disk.img -M pc
