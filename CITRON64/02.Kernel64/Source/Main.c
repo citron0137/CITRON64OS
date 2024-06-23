a3846d10df5b83d58b24ec177fb741efe5590757
rado <citron0137@gmail.com>
2024-06-23 17:10:23 +0900

    Add Keyboard Code on Main

--- CITRON64/02.Kernel64/Source/Main.c
+++ CITRON64/02.Kernel64/Source/Main.c
@@ -1,4 +1,5 @@
 #include "Types.h"
+#include "Keyboard.h"
 
 void kPrintString( int iX, int iY, const char* pcString );
 void kPrintStringWithCheckBox( int iY, const char* pcString );
@@ -6,6 +7,29 @@ void kFillCheckBox( int iY, const char* pcString );
 
 void Main( void ){
     kFillCheckBox( 10, "Pass" );
+    kPrintStringWithCheckBox(11, "Activate Keyboard");
+    if(kActiveKeyboard() == TRUE){
+        kFillCheckBox(11, "Pass");
+        kChangeKeyboardLED(FALSE, FALSE, FALSE);
+    }
+    else{
+        kFillCheckBox(11, "Fail");
+        while(1);
+    }
+
+    char vcTemp[2] = {0,};
+    BYTE bFlags, bTemp;
+    int i =0;
+    while(1){
+        if(kIsOutputBufferFull() == TRUE){
+            bTemp = kGetKeyboardScanCode();
+            if(kConvertScanCodeToASCIICode(bTemp, &(vcTemp[0]), &bFlags)==TRUE)
+                if(bFlags & KEY_FLAGS_DOWN)
+                    kPrintString(i++, 12, vcTemp);
+        }
+
+    }
+    
     while(1);
 }
 
